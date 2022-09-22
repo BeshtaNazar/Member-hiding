cd27a8384535fc69729008f52bcd4f1325c73fa4
BeshtaNazar <weirddisaster1@gmail.com>
2022-09-22 12:59:12 +0300

    initial commit

---
+++ Assets/Scripts/Humanoid.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Humanoid 
+{    
+    //Base version of the Yell method
+    public void Yell()
+    {
+        Debug.Log("Humanoid version of the Yell() method");
+    }
+    
+}
